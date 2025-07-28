41dff3091c4bd3b3eaeba1288e99f65d74dbb328
spadm7777 <spadm7777@gmail.com>
2025-07-28 11:48:31 +0900

    Use SQLite for deployment temporarily

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ class Config:
     # 개발 환경 (SQLite)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(os.getcwd(), 'instance', 'bank.db'))
     
-    # 배포 환경 (PostgreSQL)
-    if os.environ.get('DATABASE_URL'):
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-        if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
-            SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://', 1)
+    # 배포 환경에서도 SQLite 사용 (임시)
+    # if os.environ.get('DATABASE_URL'):
+    #     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    #     if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+    #         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://', 1)
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'your-secret-key'
